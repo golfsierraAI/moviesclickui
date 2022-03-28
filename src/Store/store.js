6c3b650b9f45208ee12b355dbad8e708c6fe26e1
gaurav <gsharma774962@gmail.com>
2022-03-28 15:11:14 +0530

    removing devtools

--- src/Store/store.js
+++ src/Store/store.js
@@ -2,7 +2,7 @@ import { applyMiddleware, compose, createStore } from "redux";
 import homePageSaga from "../Saga/homePageSaga";
 import rootReducer from "./rootReducer";
 import createSagaMiddleware from "redux-saga";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
 const sagaMiddleWare = createSagaMiddleware();
 const store = createStore(
